014b5961a5a4f606581a1defa5d5a0882da70a2c
Artyom Untila <untila_artem@mail.ru>
2022-12-02 17:36:42 +0300

    [Issue #1] Command "translate" supports and requires "--from" and "--to" language.

--- src/main/java/command/Command.java
+++ src/main/java/command/Command.java
@@ -9,13 +9,19 @@ public class Command {
     @CommandLine.Command(name = "translate", description = "Translate text")
     static class TranslateCommand implements Runnable {
 
+        @CommandLine.Option(names = {"-f", "--from"}, description = "From language", required = true)
+        String fromLang;
+
+        @CommandLine.Option(names = {"-t", "--to"}, description = "To language", required = true)
+        String toLang;
+
         @CommandLine.Parameters(description = "Text to translate")
         String text;
 
         @Override
         public void run() {
             try {
-                String translatedText = Translator.translate("en", "ru", text);
+                String translatedText = Translator.translate(fromLang, toLang, text);
                 System.out.println(translatedText);
             } catch (Exception e) {
                 throw new RuntimeException(e);
